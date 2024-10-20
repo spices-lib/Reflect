66d35bab82dac44f1cdcfe20bb53ae026a92cf2d
Spiecs <spiecs@outlook.com>
2024-10-20 12:23:16 +0800

    Update 03.cpp

--- src/03.cpp
+++ src/03.cpp
@@ -6,6 +6,76 @@
 #include "variable_traits.h"
 #include <iostream>
 
+class any
+{
+public:
+	enum class storage_type
+	{
+		Empty,
+		Copy,
+		Steal,
+		Ref,
+		ConstRef,
+	};
+
+	struct operations
+	{
+		any(*copy)(const any&) = {};
+		any(*steal)(any&) = {};
+		void(*release)(any&) = {};
+	};
+
+	any() = default;
+
+	any(const any& o)
+		: typeInfo_{ o.typeInfo_ }
+		, store_type{ o.store_type }
+		, ops{ o.ops }
+	{
+		if (ops.copy)
+		{
+			auto new_any = ops.copy(o);
+			payload_ = new_any.payload_;
+			new_any.payload_ = nullptr;
+			new_any.ops.release = nullptr;
+		}
+		else
+		{
+			store_type = storage_type::Empty;
+			typeInfo_ = nullptr;
+		}
+	}
+
+	any(any&& o)
+		: typeInfo_(o.typeInfo_)
+		, payload_(std::move(o.payload_))
+		, store_type(std::move(o.store_type))
+		, ops(std::move(o.ops))
+	{}
+
+	any(any&& o)
+		: typeInfo_(o.typeInfo_)
+		, payload_(std::move(o.payload_))
+		, store_type(std::move(o.store_type))
+		, ops(std::move(o.ops))
+	{}
+
+	~any()
+	{
+		if (ops.release &&
+			(store_type == storage_type::Copy ||
+				store_type == storage_type::Steal))
+		{
+			ops.release(*this);
+		}
+	}
+
+	Type* typeInfo_;
+	void* payload_;
+	storage_type store_type = storage_type::Empty;
+	operations ops;
+private:
+};
 
 struct Person final
 {
@@ -119,6 +189,18 @@ public:
 	auto GetKind() const { return kind_; }
 	bool isSigned() const { return isSigned_; }
 
+	void SetValue(double value, any& elem)
+	{
+		if (elem.typeInfo_->GetKind() == Type::Kind::Numeric)
+		{
+			switch (elem.typeInfo_->AsNumeric()->GetKind())
+			{
+			case Kind::Int8:
+				*(char*)elem.payload_ = value;
+			}
+		}
+	}
+
 	template<typename T>
 	static Numeric Create()
 	{
@@ -216,22 +298,74 @@ private:
 	std::vector<Item> items_;
 };
 
-class MemberVariable
+class Member 
+{
+public:
+	virtual any call(const std::vector<any>& anies) = 0;
+};
+
+template<typename Clazz, typename Type>
+class MemberVariable : public Member
 {
 public:
 	std::string name;
 	const Type* type;
+	Type Clazz::* ptr;
+
+	virtual any call(const std::vector<any>& anies) override
+	{
+		assert(anies.size() == 1 && anies[0].typeInfo_ == GetType<Clazz>());
+		Clazz* instance = (Clazz*)anies[0].payload_;
+		auto value = instance->*ptr;
+		return make_copy(value);
+	}
 
 	template<typename T>
 	static MemberVariable Create(const std::string& name);
 };
 
-class MemberFunction
+template<typename T, bool IsConst>
+std::conditional_t<IsConst, const T&, T&> unwarp(any& value)
+{
+	if constexpr (IsConst)
+	{
+		assert(value.typeInfo_ == GetType<T>());
+	}
+	else
+	{
+
+	}
+
+	return *(T*)value.payload_;
+}
+
+template<typename Clazz, typename RetType, size_t ...Idx, typename ...Args>
+void inner_call(RetType(Clazz::* ptr)(Args...), const std::vector<any>& params, std::index_sequence<Idx...>)
+{
+	auto return_value = ((Clazz*)params[0].payload_->*ptr)(unwarp<Args>(params[Idx+1])...);
+	return make_copy(return_value);
+}
+
+template<typename Clazz, typename Type, typename ...Args>
+class MemberFunction : public Member
 {
 public:
 	std::string name;
 	const Type* retType;
+	Type (Clazz::* ptr)(Args...);
 	std::vector<const Type*> paramType;
+
+	virtual any call(const std::vector<any>& anies) override
+	{
+		assert(anies.size() == paramType.size() + 1);
+
+		for (int i = 0; i < paramType.size(); i++)
+		{
+			assert(paramType[i] == anies[i+1].payload_);
+		}
+
+		return inner_call(ptr, anies, std::make_index_sequence<sizeof...(Args)>);
+	}
 
 	template<typename T>
 	static MemberFunction Create(const std::string& name);
@@ -433,6 +567,33 @@ std::vector<const Type*> MemberFunction::ConvertTypeList2Vector(std::index_seque
 	return { GetType<std::tuple_element_t<Idx, Params>>() ... };
 }
 
+template<typename T>
+any make_copy(const T& elem);
+
+template<typename T>
+any make_steal(T&&);
+
+template<typename T>
+any make_ref(T&);
+
+template<typename T>
+any make_cref(const T&);
+
+
+
+template<typename T>
+T* try_cast(any& elem)
+{
+	if (elem.typeInfo_ == GetType<T>())
+	{
+		return (T*)(elem.payload_);
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
 int main()
 {
 	Registrar<MyEnum>().Regist("MyEnum").Add("Value1", MyEnum::value1).Add("Value2", MyEnum::value2);
@@ -464,4 +625,151 @@ int main()
 		std::cout << std::endl;
 	}
 
+}
+
+template<typename T>
+struct operations_traits
+{
+	static any copy(const any& elem)
+	{
+		assert(elem.typeInfo_ == GetType<T>());
+
+		any return_value;
+
+		return_value.payload_   = new T{ *static_cast<T*>(elem.payload_) };
+		return_value.typeInfo_  = elem.typeInfo_;
+		return_value.store_type = any::store_type::Copy;
+		return_value.ops        = elem.ops;
+
+		return return_value;
+	}
+
+	static any steal(any& elem)
+	{
+		assert(elem.typeInfo_ == GetType<T>());
+
+		any return_value;
+
+		return_value.payload_   = new T{ std::move(*static_cast<T*>(elem.payload_)) };
+		return_value.typeInfo_  = elem.typeInfo_;
+		return_value.store_type = any::store_type::Copy;
+		elem.store_type         = any::store_type::Steal;
+		return_value.ops        = elem.ops;
+
+		return return_value;
+	}
+
+	static void release(any& elem)
+	{
+		assert(elem.typeInfo_ == GetType<T>());
+
+		delete static_cast<T*>(elem.payload_);
+		elem.payload_           = nullptr;
+		elem.store_type         = any::store_type::Empty;
+		elem.typeInfo_          = nullptr;
+	}
+};
+
+template<typename T>
+any make_copy(const T& elem)
+{
+	any return_value;
+	return_value.payload_    = new T{ elem };
+	return_value.typeInfo_   = GetType<T>();
+	return_value.store_type  = any::store_type::Copy;
+
+	if constexpr(std::is_copy_constructible_v<T>)
+	{
+		return_value.ops.copy    = &operations_traits<T>::copy;
+	}
+
+	if constexpr (std::is_move_constructible_v<T>)
+	{
+		return_value.ops.steal   = &operations_traits<T>::steal;
+	}
+
+	if constexpr (std::is_destructible_v<T>)
+	{
+		return_value.ops.release = &operations_traits<T>::release;
+	}
+
+	return return_value;
+}
+
+template<typename T>
+any make_steal(T&& elem)
+{
+	any return_value;
+	return_value.payload_    = new T{ std::move(elem) };
+	return_value.typeInfo_   = GetType<T>();
+	return_value.store_type  = any::store_type::Steal;
+
+	if constexpr(std::is_copy_constructible_v<T>)
+	{
+		return_value.ops.copy    = &operations_traits<T>::copy;
+	}
+
+	if constexpr (std::is_move_constructible_v<T>)
+	{
+		return_value.ops.steal   = &operations_traits<T>::steal;
+	}
+
+	if constexpr (std::is_destructible_v<T>)
+	{
+		return_value.ops.release = &operations_traits<T>::release;
+	}
+
+	return return_value;
+}
+
+template<typename T>
+any make_ref(T& elem)
+{
+	any return_value;
+	return_value.payload_    = &elem;
+	return_value.typeInfo_   = GetType<T>();
+	return_value.store_type  = any::store_type::Ref;
+
+	if constexpr(std::is_copy_constructible_v<T>)
+	{
+		return_value.ops.copy    = &operations_traits<T>::copy;
+	}
+
+	if constexpr (std::is_move_constructible_v<T>)
+	{
+		return_value.ops.steal   = &operations_traits<T>::steal;
+	}
+
+	if constexpr (std::is_destructible_v<T>)
+	{
+		return_value.ops.release = &operations_traits<T>::release;
+	}
+
+	return return_value;
+}
+
+template<typename T>
+any make_cref(const T& elem)
+{
+	any return_value;
+	return_value.payload_    = &elem;
+	return_value.typeInfo_   = GetType<T>();
+	return_value.store_type  = any::store_type::ConstRef;
+
+	if constexpr(std::is_copy_constructible_v<T>)
+	{
+		return_value.ops.copy    = &operations_traits<T>::copy;
+	}
+
+	if constexpr (std::is_move_constructible_v<T>)
+	{
+		return_value.ops.steal   = &operations_traits<T>::steal;
+	}
+
+	if constexpr (std::is_destructible_v<T>)
+	{
+		return_value.ops.release = &operations_traits<T>::release;
+	}
+
+	return return_value;
 }
